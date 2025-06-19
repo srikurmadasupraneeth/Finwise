fce087dd937d926f58537fe1738396006ca9f21a
Pranneth Srikurmdasu <srikurmdasupraneeth@gmail.com>
2025-06-19 16:46:18 +0530

    just added a welcome text in this

--- lib/Presentation/pages/Home/homepage.dart
+++ lib/Presentation/pages/Home/homepage.dart
@@ -1,9 +1,26 @@
+import 'package:finwise_testing_project/Common/uihelper.dart';
 import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Color(0xFF00D09E));
+    return Scaffold(
+      backgroundColor: Color(0xFF00D09E),
+      body: Stack(
+        children: [
+          Positioned(
+            left: 120,
+            top: 100,
+            child: Uihelper.customText(
+              text: "Hi, Welcome Back",
+              color: Color(0xFF052224),
+              fontsize: 20,
+              fontweight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
